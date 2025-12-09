a3b2494bb957e236009b3902f730fdaf88684ac0
Jose Machicao <jcmachicao@gestiodinamica.com>
2025-12-09 09:30:12 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,8 @@
 import os
 import openai
 
-API_KEY = os.getenv("OAIK")       # Your HF Secret
+API_KEY = os.getenv("OPENAI_API_KEY")      
 MODEL_NAME = "gpt-4"
 MAX_ITERATIONS = 6
 PREFERRED_WORD_TARGET = 200
-
 client = openai.OpenAI(api_key=API_KEY)
